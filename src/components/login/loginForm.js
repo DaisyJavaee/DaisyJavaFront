94c0842f879ef1067e7541954eb24a481240a58f
rand1925 <rand1925@outlook.com>
2020-12-31 23:23:08 +0800

    init

---
+++ src/components/login/loginForm.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import axios from 'axios'
+import {setToken} from "../../utils/auth"
+
+
+axios.defaults.headers.post['Content-Type'] = 'application/json'; //配置请求头
+
+const NormalLoginForm = () => {
+
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+    axios({
+      method: 'post',
+      url: 'user/login',
+      data: { // 这里data中的参数为requestBody参数，服务端需要使用@RequestBody注解进行获取
+        "account":values.account,
+        "password":values.password
+      }
+    })
+    .then(response=>{
+      console.log(response)
+      setToken(response.data.jwt, values.username)
+      window.alert("登陆成功")
+      window.location.href='#/home'
+    })
+    .catch(function (error) {
+      window.alert("登陆失败")
+    })
+  };
+
+  return (
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: '请输入用户名！',
+          },
+        ]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="手机号或邮箱" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: '请输入密码！',
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="密码"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>记住我</Checkbox>
+        </Form.Item>
+
+        <a className="login-form-forgot" href="">
+          忘记密码？
+        </a>
+      </Form.Item>
+        
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          登录
+        </Button>
+        &nbsp;&nbsp;&nbsp;没有账号？<a href="#/register">现在注册！</a>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default NormalLoginForm;
---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+module.exports = function (app) {
+	app.use(
+		"/api",
+		createProxyMiddleware({
+			target: "http://team.lykdsb.cn:10000",
+            changeOrigin: true
+		})
+    );
+};
